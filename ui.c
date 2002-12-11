f88e0cb2550aa4ed5f10a1475c745936aa62c8ac
cminyard <cminyard@a860ddb0-99b4-4eb6-8282-105f033f2ff5>
2002-12-11 20:16:56 +0000

    Added licensing to some files that needed it.
    git-svn-id: file:///home/cminyard/t2/svn/openipmi/trunk@41 a860ddb0-99b4-4eb6-8282-105f033f2ff5

--- ui.c
+++ ui.c
@@ -1,3 +1,35 @@
+/*
+ * ui.c
+ *
+ * MontaVista IPMI code, a simple curses UI for IPMI
+ *
+ * Author: MontaVista Software, Inc.
+ *         Corey Minyard <minyard@mvista.com>
+ *         source@mvista.com
+ *
+ * Copyright 2002 MontaVista Software Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *
+ *
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -1468,6 +1500,13 @@ mccmd_cmd(char *cmd, char **toks, void *cb_data)
     return 0;
 }
 
+int
+enable_cmd(char *cmd, char **toks, void *cb_data)
+{
+    if (
+    return 0;
+}
+
 static struct {
     char          *name;
     cmd_handler_t handler;
@@ -1479,6 +1518,7 @@ static struct {
     { "enable",				enable_cmd },
     { "controls",			controls_cmd },
     { "control",			control_cmd },
+    { "set ",				set_cmd },
     { "mcs",				mcs_cmd },
     { "mccmd",				mccmd_cmd },
     { NULL,				NULL}
