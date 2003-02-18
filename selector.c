ccaf681a229d11fef0793d481548d435fafbc593
cminyard <cminyard@a860ddb0-99b4-4eb6-8282-105f033f2ff5>
2003-02-18 18:32:34 +0000

    Split out the heap code and added a test for it.
    git-svn-id: file:///home/cminyard/t2/svn/openipmi/trunk@188 a860ddb0-99b4-4eb6-8282-105f033f2ff5

--- selector.c
+++ selector.c
@@ -60,7 +60,7 @@ typedef struct fd_control_s
     sel_fd_handler_t handle_except;
 } fd_control_t;
 
-struct sel_timer_s
+typedef struct heap_val_s
 {
     /* Set this to the function to call when the timeout occurs. */
     sel_timeout_handler_t handler;
@@ -77,10 +77,41 @@ struct sel_timer_s
 
     /* Am I currently running? */
     int in_heap;
+} heap_val_t;
 
-    /* Links for the heap. */
-    sel_timer_t *left, *right, *up;
-};
+typedef struct theap_s theap_t;
+#define heap_s theap_s
+#define heap_node_s sel_timer_s
+#define HEAP_EXPORT_NAME(s) theap_ ## s
+#define HEAP_NAMES_LOCAL static
+#define HEAP_OUTPUT_PRINTF "(%ld.%7.7ld)"
+#define HEAP_OUTPUT_DATA pos->timeout.tv_sec, pos->timeout.tv_usec
+
+static int
+cmp_timeval(struct timeval *tv1, struct timeval *tv2)
+{
+    if (tv1->tv_sec < tv2->tv_sec)
+	return -1;
+
+    if (tv1->tv_sec > tv2->tv_sec)
+	return 1;
+
+    if (tv1->tv_usec < tv2->tv_usec)
+	return -1;
+
+    if (tv1->tv_usec > tv2->tv_usec)
+	return 1;
+
+    return 0;
+}
+
+static int
+heap_cmp_key(heap_val_t *v1, heap_val_t *v2)
+{
+    return cmp_timeval(&v1->timeout, &v2->timeout);
+}
+
+#include "heap.h"
 
 struct selector_s
 {
@@ -101,7 +132,7 @@ struct selector_s
 			   this code. */
 
     /* The timer heap. */
-    volatile sel_timer_t *timer_top, *timer_last;
+    theap_t timer_heap;
 
     ipmi_lock_t *timer_lock;
 
@@ -256,24 +287,6 @@ sel_set_fd_except_handler(selector_t *sel, int fd, int state)
     ipmi_unlock(sel->fd_lock);
 }
 
-static int
-cmp_timeval(struct timeval *tv1, struct timeval *tv2)
-{
-    if (tv1->tv_sec < tv2->tv_sec)
-	return -1;
-
-    if (tv1->tv_sec > tv2->tv_sec)
-	return 1;
-
-    if (tv1->tv_usec < tv2->tv_usec)
-	return -1;
-
-    if (tv1->tv_usec > tv2->tv_usec)
-	return 1;
-
-    return 0;
-}
-
 static void
 diff_timeval(struct timeval *dest,
 	     struct timeval *left,
@@ -298,437 +311,6 @@ diff_timeval(struct timeval *dest,
     }
 }
 
-#undef MASSIVE_DEBUG
-#ifdef MASSIVE_DEBUG
-#include <stdio.h>
-FILE **debug_out = &stderr;
-static void
-print_tree_item(sel_timer_t *pos, int indent)
-{
-    int i;
-    for (i=0; i<indent; i++)
-	fprintf(*debug_out, " ");
-    fprintf(*debug_out, "  %p: %p %p %p (%ld.%7.7ld)\n", pos, pos->left, pos->right,
-	   pos->up, pos->timeout.tv_sec, pos->timeout.tv_usec);
-    if (pos->left)
-	print_tree_item(pos->left, indent+1);
-    if (pos->right)
-	print_tree_item(pos->right, indent+1);
-}
-
-static void
-print_tree(sel_timer_t *top, sel_timer_t *last)
-{
-    fprintf(*debug_out, "top=%p\n", top);
-    if (top)
-	print_tree_item(top, 0);
-    fprintf(*debug_out, "last=%p\n", last);
-    fflush(*debug_out);
-}
-
-static void
-check_tree_item(sel_timer_t *curr,
-		int         *depth,
-		int         max_depth,
-		sel_timer_t **real_last,
-		int         *found_last)
-{
-    if (! curr->left) {
-	if (curr->right) {
-	    fprintf(*debug_out, "Tree corrupt B\n");
-	    *((int *) NULL) = 0;
-	} else if (*depth > max_depth) {
-	    fprintf(*debug_out, "Tree corrupt C\n");
-	    *((int *) NULL) = 0;
-	} else if (*depth < (max_depth - 1)) {
-	    fprintf(*debug_out, "Tree corrupt D\n");
-	    *((int *) NULL) = 0;
-	} else if ((*found_last) && (*depth == max_depth)) {
-	    fprintf(*debug_out, "Tree corrupt E\n");
-	    *((int *) NULL) = 0;
-	} else if (*depth == max_depth) {
-	    *real_last = curr;
-	} else {
-	    *found_last = 1;
-	}
-    } else {
-	if (curr->left->up != curr) {
-	    fprintf(*debug_out, "Tree corrupt I\n");
-	    *((int *) NULL) = 0;
-	}
-	if (cmp_timeval(&(curr->left->timeout), &(curr->timeout)) < 0) {
-	    fprintf(*debug_out, "Tree corrupt K\n");
-	    *((int *) NULL) = 0;
-	}
-	(*depth)++;
-	check_tree_item(curr->left, depth, max_depth, real_last, found_last);
-	(*depth)--;
-
-	if (! curr->right) {
-	    if (*depth != (max_depth - 1)) {
-		fprintf(*debug_out, "Tree corrupt F\n");
-		*((int *) NULL) = 0;
-	    }
-	    if (*found_last) {
-		fprintf(*debug_out, "Tree corrupt G\n");
-		*((int *) NULL) = 0;
-	    }
-	    *found_last = 1;
-	} else {
-	    if (curr->right->up != curr) {
-		fprintf(*debug_out, "Tree corrupt H\n");
-		*((int *) NULL) = 0;
-	    }
-	    if (cmp_timeval(&(curr->right->timeout), &(curr->timeout)) < 0) {
-		fprintf(*debug_out, "Tree corrupt L\n");
-		*((int *) NULL) = 0;
-	    }
-	    (*depth)++;
-	    check_tree_item(curr->right, depth, max_depth, real_last, found_last);
-	    (*depth)--;
-	}
-    }
-}
-
-static void
-check_tree(sel_timer_t *top, sel_timer_t *last)
-{
-    unsigned int depth = 0, max_depth = 0;
-    int          found_last = 0;
-    sel_timer_t  *real_last;
-
-    if (!top) {
-	if (last) {
-	    fprintf(*debug_out, "Tree corrupt A\n");
-	    *((int *) NULL) = 0;
-	}
-	return;
-    }
-
-    real_last = top;
-    while (real_last->left) {
-	real_last = real_last->left;
-	max_depth++;
-    }
-
-    real_last = NULL;
-    check_tree_item(top, &depth, max_depth, &real_last, &found_last);
-
-    if (real_last != last) {
-	fprintf(*debug_out, "Tree corrupt J\n");
-	*((int *) NULL) = 0;
-    }
-    fflush(*debug_out);
-}
-#endif
-
-static void
-find_next_pos(sel_timer_t *curr, sel_timer_t ***next, sel_timer_t **parent)
-{
-    unsigned int upcount = 0;
-
-    if (curr->up && (curr->up->left == curr)) {
-	/* We are a left node, the next node is just my right partner. */
-	*next = &(curr->up->right);
-	*parent = curr->up;
-	return;
-    }
-
-    /* While we are a right node, go up. */
-    while (curr->up && (curr->up->right == curr)) {
-	upcount++;
-	curr = curr->up;
-    }
-
-    if (curr->up) {
-	/* Now we are a left node, trace up then back down. */
-	curr = curr->up->right;
-	upcount--;
-    }
-    while (upcount) {
-	curr = curr->left;
-	upcount--;
-    }
-    *next = &(curr->left);
-    *parent = curr;
-}
-
-static void
-find_prev_elem(sel_timer_t *curr, sel_timer_t **prev)
-{
-    unsigned int upcount = 0;
-
-    if (curr->up && (curr->up->right == curr)) {
-	/* We are a right node, the previous node is just my left partner. */
-	*prev = curr->up->left;
-	return;
-    }
-
-    /* While we are a left node, go up. */
-    while (curr->up && (curr->up->left == curr)) {
-	upcount++;
-	curr = curr->up;
-    }
-
-    if (curr->up) {
-	/* Now we are a right node, trace up then back down. */
-	curr = curr->up->left;
-    } else {
-	/* We are going to the previous "row". */
-	upcount--;
-    }
-    while (upcount) {
-	curr = curr->right;
-	upcount--;
-    }
-    *prev = curr;
-}
-
-static void
-send_up(sel_timer_t *elem, sel_timer_t **top, sel_timer_t **last)
-{
-    sel_timer_t *tmp1, *tmp2, *parent;
-
-    parent = elem->up;
-    while (parent && (cmp_timeval(&elem->timeout, &parent->timeout) < 0)) {
-	tmp1 = elem->left;
-	tmp2 = elem->right;
-	if (parent->left == elem) {
-	    elem->left = parent;
-	    elem->right = parent->right;
-	    if (elem->right)
-		elem->right->up = elem;
-	} else {
-	    elem->right = parent;
-	    elem->left = parent->left;
-	    if (elem->left)
-		elem->left->up = elem;
-	}
-	elem->up = parent->up;
-
-	if (parent->up) {
-	    if (parent->up->left == parent) {
-		parent->up->left = elem;
-	    } else {
-		parent->up->right = elem;
-	    }
-	} else {
-	    *top = elem;
-	}
-
-	parent->up = elem;
-	parent->left = tmp1;
-	if (parent->left)
-	    parent->left->up = parent;
-	parent->right = tmp2;
-	if (parent->right)
-	    parent->right->up = parent;
-
-	if (*last == elem)
-	    *last = parent;
-
-	parent = elem->up;
-    }
-}
-
-static void
-send_down(sel_timer_t *elem, sel_timer_t **top, sel_timer_t **last)
-{
-    sel_timer_t *tmp1, *tmp2, *left, *right;
-
-    left = elem->left;
-    while (left) {
-	right = elem->right;
-	/* Choose the smaller of the two below me to swap with. */
-	if ((right) && (cmp_timeval(&left->timeout, &right->timeout) > 0)) {
-
-	    if (cmp_timeval(&elem->timeout, &right->timeout) > 0) {
-		/* Swap with the right element. */
-		tmp1 = right->left;
-		tmp2 = right->right;
-		if (elem->up) {
-		    if (elem->up->left == elem) {
-			elem->up->left = right;
-		    } else {
-			elem->up->right = right;
-		    }
-		} else {
-		    *top = right;
-		}
-		right->up = elem->up;
-		elem->up = right;
-
-		right->left = elem->left;
-		right->right = elem;
-		elem->left = tmp1;
-		elem->right = tmp2;
-		if (right->left)
-		    right->left->up = right;
-		if (elem->left)
-		    elem->left->up = elem;
-		if (elem->right)
-		    elem->right->up = elem;
-
-		if (*last == right)
-		    *last = elem;
-	    } else
-		goto done;
-	} else {
-	    /* The left element is smaller, or the right doesn't exist. */
-	    if (cmp_timeval(&elem->timeout, &left->timeout) > 0) {
-		/* Swap with the left element. */
-		tmp1 = left->left;
-		tmp2 = left->right;
-		if (elem->up) {
-		    if (elem->up->left == elem) {
-			elem->up->left = left;
-		    } else {
-			elem->up->right = left;
-		    }
-		} else {
-		    *top = left;
-		}
-		left->up = elem->up;
-		elem->up = left;
-
-		left->left = elem;
-		left->right = elem->right;
-		elem->left = tmp1;
-		elem->right = tmp2;
-		if (left->right)
-		    left->right->up = left;
-		if (elem->left)
-		    elem->left->up = elem;
-		if (elem->right)
-		    elem->right->up = elem;
-
-		if (*last == left)
-		    *last = elem;
-	    } else
-		goto done;
-	}
-	left = elem->left;
-    }
-done:
-    return;
-}
-
-static void
-add_to_heap(sel_timer_t **top, sel_timer_t **last, sel_timer_t *elem)
-{
-    sel_timer_t **next;
-    sel_timer_t *parent;
-
-#ifdef MASSIVE_DEBUG
-    fprintf(*debug_out, "add_to_heap entry\n");
-    print_tree(*top, *last);
-    check_tree(*top, *last);
-#endif
-
-    elem->left = NULL;
-    elem->right = NULL;
-    elem->up = NULL;
-
-    if (*top == NULL) {
-	*top = elem;
-	*last = elem;
-	goto out;
-    }
-
-    find_next_pos(*last, &next, &parent);
-    *next = elem;
-    elem->up = parent;
-    *last = elem;
-    if (cmp_timeval(&elem->timeout, &parent->timeout) < 0) {
-	send_up(elem, top, last);
-    }
-
- out:
-#ifdef MASSIVE_DEBUG
-    fprintf(*debug_out, "add_to_heap exit\n");
-    print_tree(*top, *last);
-    check_tree(*top, *last);
-#endif
-    return;
-}
-
-static void
-remove_from_heap(sel_timer_t **top, sel_timer_t **last, sel_timer_t *elem)
-{
-    sel_timer_t *to_insert;
-
-#ifdef MASSIVE_DEBUG
-    fprintf(*debug_out, "remove_from_heap entry\n");
-    print_tree(*top, *last);
-    check_tree(*top, *last);
-#endif
-
-    /* First remove the last element from the tree, if it's not what's
-       being removed, we will use it for insertion into the removal
-       place. */
-    to_insert = *last;
-    if (! to_insert->up) {
-	/* This is the only element in the heap. */
-	*top = NULL;
-	*last = NULL;
-	goto out;
-    } else {
-	/* Set the new last position, and remove the item we will
-           insert. */
-	find_prev_elem(to_insert, last);
-	if (to_insert->up->left == to_insert) {
-	    to_insert->up->left = NULL;
-	} else {
-	    to_insert->up->right = NULL;
-	}
-    }
-
-    if (elem == to_insert) {
-	/* We got lucky and removed the last element.  We are done. */
-	goto out;
-    }
-
-    /* Now stick the formerly last element into the removed element's
-       position. */
-    if (elem->up) {
-	if (elem->up->left == elem) {
-	    elem->up->left = to_insert;
-	} else {
-	    elem->up->right = to_insert;
-	}
-    } else {
-	/* The head of the tree is being replaced. */
-	*top = to_insert;
-    }
-    to_insert->up = elem->up;
-    if (elem->left)
-	elem->left->up = to_insert;
-    if (elem->right)
-	elem->right->up = to_insert;
-    to_insert->left = elem->left;
-    to_insert->right = elem->right;
-
-    if (*last == elem)
-	*last = to_insert;
-
-    elem = to_insert;
-
-    /* Now propigate it to the right place in the tree. */
-    if (elem->up && cmp_timeval(&elem->timeout, &elem->up->timeout) < 0) {
-	send_up(elem, top, last);
-    } else {
-	send_down(elem, top, last);
-    }
-
- out:
-#ifdef MASSIVE_DEBUG
-    fprintf(*debug_out, "remove_from_head exit\n");
-    print_tree(*top, *last);
-    check_tree(*top, *last);
-#endif
-    return;
-}
-
 int
 sel_alloc_timer(selector_t            *sel,
 		sel_timeout_handler_t handler,
@@ -741,10 +323,10 @@ sel_alloc_timer(selector_t            *sel,
     if (!timer)
 	return ENOMEM;
 
-    timer->handler = handler;
-    timer->user_data = user_data;
-    timer->in_heap = 0;
-    timer->sel = sel;
+    timer->val.handler = handler;
+    timer->val.user_data = user_data;
+    timer->val.in_heap = 0;
+    timer->val.sel = sel;
     *new_timer = timer;
 
     return 0;
@@ -753,11 +335,11 @@ sel_alloc_timer(selector_t            *sel,
 int
 sel_free_timer(sel_timer_t *timer)
 {
-    ipmi_lock(timer->sel->timer_lock);
-    if (timer->in_heap) {
+    ipmi_lock(timer->val.sel->timer_lock);
+    if (timer->val.in_heap) {
 	sel_stop_timer(timer);
     }
-    ipmi_unlock(timer->sel->timer_lock);
+    ipmi_unlock(timer->val.sel->timer_lock);
     free(timer);
 
     return 0;
@@ -769,23 +351,25 @@ sel_start_timer(sel_timer_t    *timer,
 {
     volatile sel_timer_t *top;
 
-    ipmi_lock(timer->sel->timer_lock);
-    if (timer->in_heap) {
-	ipmi_unlock(timer->sel->timer_lock);
+    ipmi_lock(timer->val.sel->timer_lock);
+    if (timer->val.in_heap) {
+	ipmi_unlock(timer->val.sel->timer_lock);
 	return EBUSY;
     }
 
-    top = timer->sel->timer_top;
+    top = theap_get_top(&timer->val.sel->timer_heap);
 
-    timer->timeout = *timeout;
-    add_to_heap((sel_timer_t **) &(timer->sel->timer_top),
-		(sel_timer_t **) &(timer->sel->timer_last),
-		timer);
-    timer->in_heap = 1;
+    timer->val.timeout = *timeout;
+    theap_add(&timer->val.sel->timer_heap, timer);
+    timer->val.in_heap = 1;
 
-    if (timer->sel->send_sig && (top != timer->sel->timer_top))
-	wake_sel_thread(timer->sel);
-    ipmi_unlock(timer->sel->timer_lock);
+    if (timer->val.sel->send_sig
+	&& (top != theap_get_top(&timer->val.sel->timer_heap)))
+    {
+	/* If the top value changed, restart the waiting thread. */
+	wake_sel_thread(timer->val.sel);
+    }
+    ipmi_unlock(timer->val.sel->timer_lock);
     return 0;
 }
 
@@ -794,22 +378,24 @@ sel_stop_timer(sel_timer_t *timer)
 {
     volatile sel_timer_t *top;
 
-    ipmi_lock(timer->sel->timer_lock);
-    if (!timer->in_heap) {
-	ipmi_unlock(timer->sel->timer_lock);
+    ipmi_lock(timer->val.sel->timer_lock);
+    if (!timer->val.in_heap) {
+	ipmi_unlock(timer->val.sel->timer_lock);
 	return ETIMEDOUT;
     }
 
-    top = timer->sel->timer_top;
+    top = theap_get_top(&timer->val.sel->timer_heap);
 
-    remove_from_heap((sel_timer_t **) &(timer->sel->timer_top),
-		     (sel_timer_t **) &(timer->sel->timer_last),
-		     timer);
-    timer->in_heap = 0;
+    theap_remove(&timer->val.sel->timer_heap, timer);
+    timer->val.in_heap = 0;
 
-    if (timer->sel->send_sig && (top != timer->sel->timer_top))
-	wake_sel_thread(timer->sel);
-    ipmi_unlock(timer->sel->timer_lock);
+    if (timer->val.sel->send_sig
+	&& (top != theap_get_top(&timer->val.sel->timer_heap)))
+    {
+	/* If the top value changed, restart the waiting thread. */
+	wake_sel_thread(timer->val.sel);
+    }
+    ipmi_unlock(timer->val.sel->timer_lock);
 
     return 0;
 }
@@ -834,21 +420,19 @@ sel_select_loop(selector_t      *sel,
 
     for (;;) {
 	ipmi_lock(sel->timer_lock);
-	timer = (sel_timer_t *) sel->timer_top;
+	timer = theap_get_top(&sel->timer_heap);
 	if (timer) {
 	    /* Check for timers to time out. */
 	    gettimeofday(&now, NULL);
-	    while (cmp_timeval(&now, &timer->timeout) >= 0) {
-		remove_from_heap((sel_timer_t **) &(sel->timer_top),
-				 (sel_timer_t **) &(sel->timer_last),
-				 timer);
+	    while (cmp_timeval(&now, &timer->val.timeout) >= 0) {
+		theap_remove(&(sel->timer_heap), timer);
 
-		timer->in_heap = 0;
+		timer->val.in_heap = 0;
 		ipmi_unlock(sel->timer_lock);
-		timer->handler(sel, timer, timer->user_data);
+		timer->val.handler(sel, timer, timer->val.user_data);
 		ipmi_lock(sel->timer_lock);
 
-		timer = (sel_timer_t *) sel->timer_top;
+		timer = theap_get_top(&sel->timer_heap);
 		if (!timer) {
 		    break;
 		}
@@ -859,7 +443,7 @@ sel_select_loop(selector_t      *sel,
 	    /* Calculate how long to wait now. */
 	    gettimeofday(&now, NULL);
 	    diff_timeval((struct timeval *) to_time,
-			 (struct timeval *) &timer->timeout,
+			 (struct timeval *) &timer->val.timeout,
 			 &now);
 	} else {
 	    /* No timers, just set a long time. */
@@ -964,8 +548,7 @@ sel_alloc_selector(selector_t **new_selector)
 	init_fd((fd_control_t *) &(sel->fds[i]));
     }
 
-    sel->timer_top = NULL;
-    sel->timer_last = NULL;
+    theap_init(&sel->timer_heap);
 
     *new_selector = sel;
 
@@ -980,31 +563,22 @@ sel_alloc_selector(selector_t **new_selector)
     return rv;
 }
 
-static void
-free_heap_element(sel_timer_t *elem)
-{
-    if (!elem)
-	return;
-
-    free_heap_element(elem->left);
-    free_heap_element(elem->right);
-    free(elem);
-}
-
 int
 sel_free_selector(selector_t *sel)
 {
-    sel_timer_t *heap;
+    sel_timer_t *elem;
 
     if (sel->timer_lock)
 	ipmi_destroy_lock(sel->timer_lock);
     if (sel->fd_lock)
 	ipmi_destroy_lock(sel->fd_lock);
 
-    heap = (sel_timer_t *) sel->timer_top;
-
+    elem = theap_get_top(&(sel->timer_heap));
+    while (elem) {
+	theap_remove(&(sel->timer_heap), elem);
+	free(elem);
+    }
     free(sel);
-    free_heap_element(heap);
 
     return 0;
 }
---
+++ test_heap.c
@@ -0,0 +1,148 @@
+/*
+ * test_heap.c
+ *
+ * Code to test the heap.
+ *
+ * Author: MontaVista Software, Inc.
+ *         Corey Minyard <minyard@mvista.com>
+ *         source@mvista.com
+ *
+ * Copyright 2002 MontaVista Software Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *
+ *
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include <malloc.h>
+
+#define HEAP_EXPORT_NAME(s) test_ ## s
+typedef struct heap_val_s { int a; } heap_val_t;
+#define heap_node_s test_heap_node_s
+#define heap_s test_heap_s
+
+typedef struct test_heap_node_s test_heap_node_t;
+typedef struct test_heap_s test_heap_t;
+
+int debug = 0;
+
+int
+heap_cmp_key(heap_val_t *val1, heap_val_t *val2)
+{
+    if (val1->a < val2->a) {
+	return -1;
+    } else if (val1->a > val2->a) {
+	return 1;
+    } else {
+	return 0;
+    }
+}
+#define HEAP_OUTPUT_PRINTF "(%d)"
+#define HEAP_OUTPUT_DATA pos->val.a
+
+#define HEAP_DEBUG
+
+#include "heap.h"
+
+static int random_seed;
+
+void
+handle_fault(int sig)
+{
+    fprintf(stderr, "Died on sig %d\n", sig);
+    printf("Seed was %d\n", random_seed);
+    exit(1);
+}
+
+#define TEST_SIZE 2048
+test_heap_node_t *(nodes[TEST_SIZE]);
+
+int
+main(int argc, char *argv[])
+{
+    test_heap_t      heap;
+    int              i;
+    int              err;
+    test_heap_node_t *val1;
+    struct sigaction act;
+    int              rand_val;
+
+    i = 1;
+    while ((i < argc) && (argv[i][0] == '-')) {
+	if (strcmp(argv[i], "--") == 0)
+	    break;
+	else if (strcmp(argv[i], "-d") == 0)
+	    debug++;
+	else {
+	    fprintf(stderr, "Invalid option: '%s'\n", argv[i]);
+	    exit(1);
+	}
+	
+	i++;
+    }
+    if (i < argc) {
+	random_seed = atoi(argv[i]);
+    } else {
+	random_seed = time(NULL);
+    }
+    if (debug)
+	printf("Random seed is %d\n", random_seed);
+    srand(random_seed);
+    test_debug_out = &stdout;
+
+    act.sa_handler = handle_fault;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    err = sigaction(SIGSEGV, &act, NULL);
+    if (err) {
+	perror("sigaction");
+    }
+
+    test_init(&heap);
+
+    for (i=0; i<TEST_SIZE; i++) {
+	rand_val = rand() & (TEST_SIZE-1);
+	if (nodes[rand_val]) {
+	    if (debug > 1)
+		printf("Removing item %d\n", nodes[rand_val]->val.a);
+	    test_remove(&heap, nodes[rand_val]);
+	    free(nodes[rand_val]);
+	    nodes[rand_val] = NULL;
+	} else {
+	    val1 = malloc(sizeof(*val1));
+	    val1->val.a = rand();
+	    if (debug > 1)
+		printf("Adding item %d\n", val1->val.a);
+	    test_add(&heap, val1);
+	    nodes[rand_val] = val1;
+	}
+	test_check(&heap);
+    }
+    if (debug > 1)
+	test_print(&heap);
+    if (debug)
+	printf("Seed was %d\n", random_seed);
+
+    return 0;
+}
