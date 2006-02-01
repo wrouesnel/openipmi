d61f623550737f75bca1f853e98611e7ec86275a
cminyard <cminyard@a860ddb0-99b4-4eb6-8282-105f033f2ff5>
2006-02-01 04:49:27 +0000

    Lots of work on the terminal.
    git-svn-id: file:///home/cminyard/t2/svn/openipmi/trunk@1526 a860ddb0-99b4-4eb6-8282-105f033f2ff5

--- swig/python/openipmigui/_term.py
+++ swig/python/openipmigui/_term.py
@@ -33,6 +33,8 @@
 import wx
 import wx.lib.colourdb
 
+id_st = 1200
+
 black = 0
 red = 1
 green = 2
@@ -61,6 +63,7 @@ class TerminalEmulator:
         self.y = 0
         self.height = 24
         self.width = 80
+        self.InputHandler = self.Input0
         return
 
     def check_scroll(self):
@@ -76,13 +79,7 @@ class TerminalEmulator:
                 pass
             pass
         if (redraw):
-            for i in range(0, self.height):
-                s = ""
-                for j in range(0, self.width):
-                    s += self.buf[i][j]
-                    pass
-                self.DrawText(white, black, 0, 0, i, s)
-                pass
+            self.ScrollLines(0, self.height-1)
             pass
         return
 
@@ -114,56 +111,165 @@ class TerminalEmulator:
             pass
         return
         
-    def output_data(self, len):
+    def output_str(self, s):
+        if (len(s) == 0):
+            return
+        do_scroll = False
         if (self.x == self.width):
             # Cursor is at the end of the line, redraw the char where
             # the cursor is sitting.
             self.restore_cursor()
+            self.x = 0;
+            self.y += 1;
+            self.check_scroll()
             pass
-        if (len == 0):
-            return
-        self.output_at(self.x, self.y, len)
-        self.x += len
+        while ((self.x + len(s)) >= self.width):
+            # The string exceeds the line length, do it in pieces
+            outlen = self.width - self.x
+            do_scroll = True
+            self.buf[self.y][self.x:self.x+outlen] = s[0:outlen]
+            self.output_at(self.x, self.y, outlen)
+            self.x = 0;
+            self.y += 1;
+            s = s[outlen:]
+            self.check_scroll()
+            pass
+        outlen = len(s)
+        self.buf[self.y][self.x:self.x+outlen] = s[0:outlen]
+        self.output_at(self.x, self.y, outlen)
+        self.x += outlen
         self.handle_cursor()
         return
 
-    def ProcessInput(self, data):
-        pos = 0
-        while (len(data) > 0):
-            if (pos >= len(data)):
-                self.output_data(pos)
-                data = ""
-                pass
-            elif (data[pos] == '\n'):
-                self.output_data(pos)
+    # After '\e['
+    def Input2(self, c, s):
+        if (c == 'A'): # Up
+            if (self.y > 0):
                 self.restore_cursor()
-                data = data[pos+1:]
-                self.y += 1
-                self.check_scroll()
+                self.y -= 1
                 self.handle_cursor();
-                pos = 0
-                pass
-            elif (data[pos] == '\r'):
-                self.output_data(pos)
-                self.restore_cursor()
-                data = data[pos+1:]
-                self.x = 0
-                self.handle_cursor();
-                pos = 0
-                pass
-            elif ((pos + self.x) >= self.width):
-                self.output_data(pos)
-                self.x = 0
-                self.y += 1
-                self.check_scroll()
-                data = data[pos:]
-                pos = 0
-                pass
-            else:
-                self.buf[self.y][self.x+pos] = data[pos]
-                pos += 1
                 pass
             pass
+        elif (c == 'B'): # Down
+            if (self.y < (self.height-1)):
+                self.restore_cursor()
+                self.y += 1
+                self.handle_cursor();
+                pass
+            pass
+        elif (c == 'C'): # Right
+            if (self.x < (self.width-1)):
+                self.restore_cursor()
+                self.x += 1
+                self.handle_cursor();
+                pass
+            pass
+        elif (c == 'D'): # Right
+            if (self.x > 0):
+                self.restore_cursor()
+                self.x -= 1
+                self.handle_cursor();
+                pass
+            pass
+        elif ((c >= '0') and (c <= '9')):
+            if (self.parms == None):
+                self.parms = [ int(c) ]
+            else:
+                currparm = len(self.parms) - 1
+                self.parms[currparm] *= 10
+                self.parms[currparm] += int(c)
+                pass
+            return "" # Stay in Input2
+        elif (c == ';'): # Right
+            if (self.parms == None):
+                self.parms = [ 0, 0 ]
+            else:
+                self.parms.append(0)
+                pass
+            return "" # Stay in Input2
+        elif (c == 'H'): # Move to position specified by self.parms
+            if (self.parms == None):
+                x = 1
+                y = 1
+                pass
+            elif (len(self.parms) == 1):
+                y = self.parms[0]
+                x = 1
+                pass
+            else:
+                y = self.parms[0]
+                x = self.parms[1]
+                pass
+            if (x < 1):
+                x = 1
+            elif (x > self.width):
+                x = self.width
+                pass
+            self.restore_cursor()
+            self.x = x - 1
+            self.y = y - 1
+            self.handle_cursor();
+            pass
+        self.InputHandler = self.Input0
+        return ""
+
+    # After an escape
+    def Input1(self, c, s):
+        if (c == '['):
+            self.parms = None
+            self.InputHandler = self.Input2
+            pass
+        else:
+            self.InputHandler = self.Input0
+            pass
+        return ""
+
+    # "normal" input mode
+    def Input0(self, c, s):
+        if ((c >= ' ') and (c <= '~')):
+            return s + c
+        else:
+            if (c == '\n'):
+                self.restore_cursor()
+                self.y += 1
+                self.check_scroll()
+                self.handle_cursor();
+                pass
+            elif (c == '\r'):
+                self.restore_cursor()
+                self.x = 0
+                self.handle_cursor();
+                pass
+            elif (c == '\t'):
+                self.restore_cursor()
+                if (self.x >= self.width-8):
+                    self.x = self.width-1
+                else:
+                    self.x = ((self.x / 8) * 8) + 8
+                    pass
+                self.handle_cursor();
+                pass
+            elif (c == '\007'): #bell
+                self.Bell()
+            elif (c == '\010'): #backspace
+                if (self.x > 0):
+                    self.restore_cursor()
+                    self.x -= 1
+                    self.handle_cursor();
+                    pass
+                pass
+            elif (c == '\x1b'):
+                self.InputHandler = self.Input1
+                pass
+            pass
+        return ""
+    
+    def ProcessInput(self, data):
+        s = ""
+        for c in data:
+            s = self.InputHandler(c, s)
+            pass
+        self.output_str(s)
         return
 
     def ResizeTerminal(self, w, h):
@@ -193,13 +299,11 @@ class TerminalEmulator:
 
 class Terminal(TerminalEmulator):
     def __init__(self, parent):
-        self.textctrl = wx.TextCtrl(parent, -1,
-                                    style=(wx.TE_MULTILINE | wx.HSCROLL
-                                           | wx.TE_READONLY))
-        
         self.style = wx.TextAttr()
 
-        self.fonts = (wx.Font(pointSize = 10,
+        self.fsize = 8
+
+        self.fonts = (wx.Font(pointSize = self.fsize,
                               family = wx.FONTFAMILY_TELETYPE,
                               style = wx.FONTSTYLE_NORMAL,
                               weight = wx.FONTWEIGHT_NORMAL,
@@ -207,7 +311,7 @@ class Terminal(TerminalEmulator):
                               faceName = "",
                               encoding = wx.FONTENCODING_ISO8859_1),
                       # Bold
-                      wx.Font(pointSize = 10,
+                      wx.Font(pointSize = self.fsize,
                               family = wx.FONTFAMILY_TELETYPE,
                               style = wx.FONTSTYLE_NORMAL,
                               weight = wx.FONTWEIGHT_BOLD,
@@ -215,7 +319,7 @@ class Terminal(TerminalEmulator):
                               faceName = "",
                               encoding = wx.FONTENCODING_ISO8859_1),
                       # Underline
-                      wx.Font(pointSize = 10,
+                      wx.Font(pointSize = self.fsize,
                               family = wx.FONTFAMILY_TELETYPE,
                               style = wx.FONTSTYLE_NORMAL,
                               weight = wx.FONTWEIGHT_NORMAL,
@@ -223,7 +327,7 @@ class Terminal(TerminalEmulator):
                               faceName = "",
                               encoding = wx.FONTENCODING_ISO8859_1),
                       # Bold Underline
-                      wx.Font(pointSize = 10,
+                      wx.Font(pointSize = self.fsize,
                               family = wx.FONTFAMILY_TELETYPE,
                               style = wx.FONTSTYLE_NORMAL,
                               weight = wx.FONTWEIGHT_BOLD,
@@ -240,7 +344,18 @@ class Terminal(TerminalEmulator):
                        wx.CYAN,
                        wx.WHITE)
 
+        self.textctrl = wx.TextCtrl(parent, -1,
+                                    style=(wx.TE_MULTILINE | wx.HSCROLL
+                                           | wx.TE_READONLY))
+
+        (self.charwidth, self.charheight,
+         descent, eleading) = self.textctrl.GetFullTextExtent(" ",
+                                                              self.fonts[0])
+        
         TerminalEmulator.__init__(self)
+
+        self.xpixels = self.charwidth * self.width
+        self.ypixels = self.charheight * self.height
 
         for i in range(0, self.height-1):
             self.textctrl.AppendText("%*s\n" % (self.width, "") )
@@ -251,7 +366,7 @@ class Terminal(TerminalEmulator):
 
         wx.EVT_CHAR(self.textctrl, self.HandleChar)
         return
-
+    
     def DrawText(self, fg_color, bg_color, flags, x, y, val):
         pos = (y * (self.width+1)) + x
         if (flags & INVERSE):
@@ -272,6 +387,18 @@ class Terminal(TerminalEmulator):
         # Draw cursor as inverse, just flip the background and foreground
         self.DrawText(bg_color, fg_color, flags, x, y, val)
         return
+
+    def ScrollLines(self, y1, y2):
+        self.textctrl.Remove(y1 * (self.width+1), (y1+1) * (self.width+1))
+        if (y2 == self.height-1):
+            self.textctrl.AppendText("\n%*s" % (self.width, "") )
+            pass
+        else:
+            pos = (y2+1) * (self.width+1)
+            self.textctrl.Replace(pos, pos, "%*s\n" % (self.width, "") )
+            pass
+        self.ExposeArea(0, y2, self.width, 1)
+        return
     
     def SendBack(self, data):
         self.HandleTerminalOutput(data)
@@ -288,6 +415,14 @@ class Terminal(TerminalEmulator):
         if (key <= 255):
             s = "%c" % (key)
             pass
+        elif (key == wx.WXK_UP):
+            s = "\x1b[A"
+        elif (key == wx.WXK_DOWN):
+            s = "\x1b[B"
+        elif (key == wx.WXK_RIGHT):
+            s = "\x1b[C"
+        elif (key == wx.WXK_LEFT):
+            s = "\x1b[D"
         else:
             return
         self.HandleTerminalOutput(s)
@@ -295,17 +430,38 @@ class Terminal(TerminalEmulator):
     
     pass
 
-#class MyApp(wx.App):
-#    def __init__(self):
-#        self.name = "app"
-#        wx.App.__init__(self);
-#
-#    def OnInit(self):
-#        frame = wx.Frame(None, -1)
-#        term = Terminal(frame)
-#        self.SetTopWindow(frame)
-#        frame.Show(True)
-#        return True
-#
-#app = MyApp()
-#app.MainLoop()
+import sys
+class TestTerm(Terminal):
+    def __init__(self, parent):
+        Terminal.__init__(self, parent)
+        return
+
+    def HandleTerminalOutput(self, s):
+        # FIXME: hacks to remove
+        sys.stdout.write(s)
+        sys.stdout.flush()
+        if (s[0] == '\x01'):
+            self.ProcessInput("abcdefghijk")
+            return
+        self.ProcessInput(s)
+        return
+    pass
+
+class MyApp(wx.App):
+    def __init__(self):
+        self.name = "app"
+        wx.App.__init__(self);
+
+    def OnInit(self):
+        frame = wx.Frame(None, -1)
+        term = TestTerm(frame)
+        frame.SetSizeWH(term.xpixels+30, term.ypixels+30)
+        self.SetTopWindow(frame)
+        frame.Show(True)
+        return True
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.MainLoop()
+
+
