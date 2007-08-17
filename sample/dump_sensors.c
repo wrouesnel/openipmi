90549eec8a94debe6d732fc1c5534889953f7adc
cminyard <cminyard@a860ddb0-99b4-4eb6-8282-105f033f2ff5>
2007-08-17 16:10:13 +0000

    Added another sample program.
    git-svn-id: file:///home/cminyard/t2/svn/openipmi/trunk@1801 a860ddb0-99b4-4eb6-8282-105f033f2ff5

---
+++ sample/dump_sensors.c
@@ -0,0 +1,267 @@
+/*
+ * dump_sensors.c
+ *
+ * OpenIPMI test code
+ *
+ * Author: Corey Minyard <minyard@acm.org>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *
+ *
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <time.h>
+
+#include <OpenIPMI/ipmiif.h>
+#include <OpenIPMI/ipmi_smi.h>
+#include <OpenIPMI/ipmi_err.h>
+#include <OpenIPMI/ipmi_auth.h>
+#include <OpenIPMI/ipmi_lan.h>
+#include <OpenIPMI/ipmi_posix.h>
+#include <OpenIPMI/ipmi_fru.h>
+
+#include <OpenIPMI/internal/ipmi_sensor.h>
+
+static const char *progname;
+
+static void con_usage(const char *name, const char *help, void *cb_data)
+{
+    printf("\n%s%s", name, help);
+}
+
+static void
+usage(void)
+{
+    printf("Usage:\n");
+    printf(" %s <con_parms>\n", progname);
+    printf(" Where <con_parms> is one of:");
+    ipmi_parse_args_iter_help(con_usage, NULL);
+}
+
+static int done;
+
+/* Whenever the status of a sensor changes, the function is called
+   We display the information of the sensor if we find a new sensor
+*/
+static void
+sensor_change(enum ipmi_update_e op,
+	      ipmi_entity_t      *ent,
+	      ipmi_sensor_t      *sensor,
+	      void               *cb_data)
+{
+    int id, instance;
+    char name[33];
+
+    id = ipmi_entity_get_entity_id(ent);
+    instance = ipmi_entity_get_entity_instance(ent);
+    ipmi_sensor_get_id(sensor, name, 32);
+    if (op == IPMI_ADDED) {
+	int lun, num;
+	printf("Sensor added: %d.%d.%s\n", id, instance, name);
+	ipmi_sensor_get_num(sensor, &lun, &num);
+	printf("  owner:   %d\n", ipmi_sensor_get_owner(sensor));
+	printf("  channel: %d\n", ipmi_sensor_get_channel(sensor));
+	printf("  lun:     %d\n", lun);
+	printf("  num:     %d\n", num);
+    }
+}
+
+/* Whenever the status of an entity changes, the function is called
+   When a new entity is created, we search all sensors that belong 
+   to the entity */
+static void
+entity_change(enum ipmi_update_e op,
+	      ipmi_domain_t      *domain,
+	      ipmi_entity_t      *entity,
+	      void               *cb_data)
+{
+    int rv;
+    int id, instance;
+
+    id = ipmi_entity_get_entity_id(entity);
+    instance = ipmi_entity_get_entity_instance(entity);
+    if (op == IPMI_ADDED) {
+	printf("Entity added: %d.%d\n", id, instance);
+	/* Register callback so that when the status of a
+	   sensor changes, sensor_change is called */
+	rv = ipmi_entity_add_sensor_update_handler(entity,
+						   sensor_change,
+						   entity);
+	if (rv) {
+	    printf("ipmi_entity_set_sensor_update_handler: 0x%x", rv);
+	    exit(1);
+	}
+    }
+}
+
+/* After we have established connection to domain, this function get called
+   At this time, we can do whatever things we want to do. Herr we want to
+   search all entities in the system */ 
+void
+setup_done(ipmi_domain_t *domain,
+	   int           err,
+	   unsigned int  conn_num,
+	   unsigned int  port_num,
+	   int           still_connected,
+	   void          *user_data)
+{
+    int rv;
+
+    /* Register a callback function entity_change. When a new entities 
+       is created, entity_change is called */
+    rv = ipmi_domain_add_entity_update_handler(domain, entity_change, domain);
+    if (rv) {      
+	printf("ipmi_domain_add_entity_update_handler return error: %d\n", rv);
+	return;
+    }
+
+}
+
+static void
+domain_closed(void *cb_data)
+{
+    done = 1;
+}
+
+static void
+domain_up(ipmi_domain_t *domain, void *cb_data)
+{
+    int rv;
+
+    rv = ipmi_domain_close(domain, domain_closed, NULL);
+    if (rv) {      
+	printf("ipmi_domain_close return error: %d\n", rv);
+	exit(1);
+    }
+}
+
+static os_handler_t *os_hnd;
+
+static void
+my_vlog(os_handler_t         *handler,
+	const char           *format,
+	enum ipmi_log_type_e log_type,
+	va_list              ap)
+{
+    int do_nl = 1;
+
+    switch(log_type)
+    {
+	case IPMI_LOG_INFO:
+	    printf("INFO: ");
+	    break;
+
+	case IPMI_LOG_WARNING:
+	    printf("WARN: ");
+	    break;
+
+	case IPMI_LOG_SEVERE:
+	    printf("SEVR: ");
+	    break;
+
+	case IPMI_LOG_FATAL:
+	    printf("FATL: ");
+	    break;
+
+	case IPMI_LOG_ERR_INFO:
+	    printf("EINF: ");
+	    break;
+
+	case IPMI_LOG_DEBUG_START:
+	    do_nl = 0;
+	    /* FALLTHROUGH */
+	case IPMI_LOG_DEBUG:
+	    printf("DEBG: ");
+	    break;
+
+	case IPMI_LOG_DEBUG_CONT:
+	    do_nl = 0;
+	    /* FALLTHROUGH */
+	case IPMI_LOG_DEBUG_END:
+	    break;
+    }
+
+    vprintf(format, ap);
+
+    if (do_nl)
+	printf("\n");
+}
+
+int
+main(int argc, char *argv[])
+{
+    int         rv;
+    int         curr_arg = 1;
+    ipmi_args_t *args;
+    ipmi_con_t  *con;
+
+    progname = argv[0];
+
+    /* OS handler allocated first. */
+    os_hnd = ipmi_posix_setup_os_handler();
+    if (!os_hnd) {
+	printf("ipmi_smi_setup_con: Unable to allocate os handler\n");
+	exit(1);
+    }
+
+    /* Override the default log handler (just to show how). */
+    os_hnd->set_log_handler(os_hnd, my_vlog);
+
+    /* Initialize the OpenIPMI library. */
+    ipmi_init(os_hnd);
+
+    rv = ipmi_parse_args2(&curr_arg, argc, argv, &args);
+    if (rv) {
+	fprintf(stderr, "Error parsing command arguments, argument %d: %s\n",
+		curr_arg, strerror(rv));
+	usage();
+	exit(1);
+    }
+
+    rv = ipmi_args_setup_con(args, os_hnd, NULL, &con);
+    if (rv) {
+        fprintf(stderr, "ipmi_ip_setup_con: %s", strerror(rv));
+	exit(1);
+    }
+
+    rv = ipmi_open_domain("", &con, 1, setup_done, NULL, domain_up, NULL,
+			  NULL, 0, NULL);
+    if (rv) {
+	fprintf(stderr, "ipmi_init_domain: %s\n", strerror(rv));
+	exit(1);
+    }
+
+    /* We run the select loop here, this shows how you can use
+       sel_select.  You could add your own processing in this loop. */
+    while (!done)
+	os_hnd->perform_one_op(os_hnd, NULL);
+
+    os_hnd->free_os_handler(os_hnd);
+}
