65ede69d5898994357e824e2187b37e1f850931a
Corey Minyard <cminyard@mvista.com>
2017-01-04 15:04:49 -0600

    Add perl dummystuff.c
    This was missed in a previous commit.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ swig/perl/dummystuff.c
@@ -0,0 +1,10 @@
+/*
+ * Supply some dummy functions that are needed for linking but not
+ * used.
+ */
+
+#include <OpenIPMI/ipmi_cmdlang.h>
+
+void ipmi_cmdlang_report_event(ipmi_cmdlang_event_t *event)
+{
+}
