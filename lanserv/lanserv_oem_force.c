d589f257d97593e3a139506a7b8d3678e3f7e1da
Corey Minyard <cminyard@mvista.com>
2017-01-26 13:31:34 -0600

    Add BSD licenses in files where it was missing.
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- lanserv/lanserv_oem_force.c
+++ lanserv/lanserv_oem_force.c
@@ -29,6 +29,22 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free
  *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Modified BSD Licence
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *   3. The name of the author may not be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
  */
 
 #include <stdlib.h>
