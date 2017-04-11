wrouesnel/openipmi

commit 098038d46fb26362bcbc9fae41b4e6e35bedc9a7

Fixes for more problems found by Coverity
Signed-off-by: Corey Minyard <cminyard@mvista.com>

lanserv/persist.c
@@ -95,6 +95,7 @@ persist_init(const char *papp, const char *instance, const char *ibasedir)
     char *dname;
     struct stat st;
     char *n;
+    int rv = 0;
 
     if (app)
 	return EBUSY;
@@ -111,11 +112,14 @@ persist_init(const char *papp, const char *instance, const char *ibasedir)
 
     len = strlen(basedir) + strlen(app) + 3;
     dname = malloc(len);
-    if (!dname)
+    if (!dname) {
+	free(app);
 	return ENOMEM;
+    }
     strcpy(dname, basedir);
     strcat(dname, "/");
     strcat(dname, app);
+    free(app);
     strcat(dname, "/");
     if (dname[0] == '/')
 	n = strchr(dname + 1, '/');
@@ -124,14 +128,20 @@ persist_init(const char *papp, const char *instance, const char *ibasedir)
     while (n) {
 	*n = '\0';
 	if (stat(dname, &st) != 0) {
-	    if (mkdir(dname, 0755) != 0)
-		return errno;
-	} else if (!S_ISDIR(st.st_mode))
-	    return ENOTDIR;
+	    if (mkdir(dname, 0755) != 0) {
+		rv = errno;
+		goto out;
+	    }
+	} else if (!S_ISDIR(st.st_mode)) {
+	    rv = ENOTDIR;
+	    goto out;
+	}
 	*n++ = '/';
 	n = strchr(n, '/');
     }
-    return 0;
+ out:
+    free(dname);
+    return rv;
 }
 
 static char *

lanserv/config.c
@@ -304,8 +304,12 @@ get_delim_str(char **rtokptr, char **rval, const char **err)
 	    oldc = *tokptr;
 	    *tokptr = '\0';
 	    val = find_variable(val);
-	    if (!val)
+	    if (!val) {
+		if (rv)
+		    free(rv);
+		*err = "unable to find variable";
 		return -1;
+	    }
 	    *tokptr = oldc;
 	} else if (isquote(*tokptr)) {
 	    endc = *tokptr;
@@ -321,6 +325,8 @@ get_delim_str(char **rtokptr, char **rval, const char **err)
 	    *tokptr = '\0';
 	    tokptr++;
 	} else {
+	    if (rv)
+		free(rv);
 	    *err = "string value must start with '\"' or '''";
 	    return -1;
 	}
@@ -328,6 +334,7 @@ get_delim_str(char **rtokptr, char **rval, const char **err)
 	if (rv) {
 	    char *newrv = malloc(strlen(rv) + strlen(val) + 1);
 	    if (!newrv) {
+		free(rv);
 		*err = "Out of memory copying string";
 		return -1;
 	    }
@@ -691,6 +698,7 @@ load_dynamic_libs(sys_data_t *sys, int print_version)
 	    if (func) {
 		err = func(sys, dlib->init);
 		if (err) {
+		    dlclose(handle);
 		    fprintf(stderr, "Error from module %s version print: %s\n",
 			    dlib->file, strerror(err));
 		    return EINVAL;
@@ -702,6 +710,7 @@ load_dynamic_libs(sys_data_t *sys, int print_version)
 	    if (func) {
 		err = func(sys, dlib->init);
 		if (err) {
+		    dlclose(handle);
 		    fprintf(stderr, "Error from module %s init: %s\n",
 			    dlib->file, strerror(err));
 		    return EINVAL;

lanserv/extcmd.c
@@ -279,7 +279,7 @@ extcmd_getvals(sys_data_t *sys,
 {
     int rv;
     char *cmd;
-    FILE *f;
+    FILE *f = NULL;
     unsigned int i;
     char buf[2048];
     unsigned int buflen = sizeof(buf);
@@ -321,6 +321,7 @@ extcmd_getvals(sys_data_t *sys,
     buf[rv] = '\0';
 
     rv = pclose(f);
+    f = NULL;
     if (rv) {
 	sys->log(sys, OS_ERROR, NULL, 
 		 "extcmd read command (%s) failed: %x: %s", cmd, rv, buf);
@@ -337,6 +338,8 @@ extcmd_getvals(sys_data_t *sys,
 	}
     }
   out:
+    if (f)
+	pclose(f);
     free(cmd);
     return rv;
 }
@@ -348,7 +351,7 @@ extcmd_setvals(sys_data_t *sys,
 {
     int rv = 0;
     char *cmd;
-    FILE *f;
+    FILE *f = NULL;
     unsigned int i;
     char buf[2048];
     unsigned int buflen = sizeof(buf);
@@ -397,6 +400,7 @@ extcmd_setvals(sys_data_t *sys,
     buf[rv] = '\0';
 
     rv = pclose(f);
+    f = NULL;
     if (rv) {
 	sys->log(sys, OS_ERROR, NULL, 
 		 "extcmd write command (%s) failed: %x: %s", cmd, rv, buf);
@@ -404,6 +408,8 @@ extcmd_setvals(sys_data_t *sys,
     }
 
   out:
+    if (f)
+	pclose(f);
     free(cmd);
     return rv;
 }
@@ -415,7 +421,7 @@ extcmd_checkvals(sys_data_t *sys,
 {
     int rv = 0;
     char *cmd;
-    FILE *f;
+    FILE *f = NULL;
     unsigned int i;
     char buf[2048];
     unsigned int buflen = sizeof(buf);
@@ -463,8 +469,11 @@ extcmd_checkvals(sys_data_t *sys,
 
     /* Return value should tell us if it's ok. */
     rv = pclose(f);
+    f = NULL;
 
   out:
+    if (f)
+	pclose(f);
     free(cmd);
     return rv;
 }

lanserv/serial_ipmi.c
@@ -1258,7 +1258,7 @@ serserv_read_config(char **tokptr, sys_data_t *sys, const char **errstr)
     err = get_sock_addr(tokptr, &ser->addr.addr, &ser->addr.addr_len,
 			NULL, SOCK_STREAM, errstr);
     if (err)
-	return err;
+	goto out_err;
 
     for (tok = mystrtok(NULL, " \t\n", tokptr); tok;
 	 tok = mystrtok(NULL, " \t\n", tokptr)) {

lib/domain.c
@@ -2738,7 +2738,7 @@ ipmi_start_ipmb_mc_scan(ipmi_domain_t  *domain,
 
     CHECK_DOMAIN_LOCK(domain);
 
-    if (channel > MAX_IPMI_USED_CHANNELS)
+    if (channel >= MAX_IPMI_USED_CHANNELS)
 	return EINVAL;
 
     if ((domain->chan[channel].medium != 1)

swig/OpenIPMI.i
@@ -2248,7 +2248,7 @@ light_setting_to_str(ipmi_light_setting_t *e)
     char dummy[1];
     int  size = 0;
 
-    for (i=0; i<count; i++) {
+    for (i = 0; i < count; i++) {
 	int val;
 	size += 1; /* For the colon */
 	val = 0;
@@ -2266,10 +2266,12 @@ light_setting_to_str(ipmi_light_setting_t *e)
 	size += snprintf(dummy, 1, "%d ", val);
     }
 
-    str = malloc(size+1);
+    str = malloc(size + 1);
+    if (!str)
+	return NULL;
     s = str;
 
-    for (i=0; i<count; i++) {
+    for (i = 0; i < count; i++) {
 	int val;
 	const char *ov;
 
@@ -2363,10 +2365,13 @@ control_val_get_light_handler(ipmi_control_t *control, int err,
 {
     swig_cb_val cb = cb_data;
     swig_ref    control_ref;
+    char        *str;
 
     control_ref = swig_make_ref(control, ipmi_control_t);
-    swig_call_cb(cb, "control_get_light_cb", "%p%d%s", &control_ref, err,
-		 light_setting_to_str(val));
+    str = light_setting_to_str(val);
+    if (!str)
+	str = "err";
+    swig_call_cb(cb, "control_get_light_cb", "%p%d%s", &control_ref, err, str);
     swig_free_ref_check(control_ref, ipmi_control_t);
     /* One-time call, get rid of the CB. */
     deref_swig_cb_val(cb);
@@ -7480,6 +7485,7 @@ ipmi_args_t *alloc_parse_args(argarray *args);
 	    goto out_err;
 	if (!nil_swig_cb(handler)) {
 	    if (! valid_swig_cb(handler, sensor_event_enable_cb)) {
+		free(st);
 		rv = EINVAL;
 		goto out_err;
 	    }
@@ -7570,9 +7576,9 @@ ipmi_args_t *alloc_parse_args(argarray *args);
 			       sensor_cb, handler_val);
 	if (rv && handler_val)
 	    deref_swig_cb_val(handler_val);
+    out_err:
 	if (st)
 	    free(st);
-    out_err:
 	IPMI_SWIG_C_CB_EXIT
 	return rv;
     }
@@ -7682,7 +7688,7 @@ ipmi_args_t *alloc_parse_args(argarray *args);
 	if (!nil_swig_cb(handler)) {
 	    if (! valid_swig_cb(handler, sensor_set_thresholds_cb)) {
 		rv = EINVAL;
-		goto out_err;
+		goto out_err_free;
 	    }
 	    sensor_cb = sensor_set_thresholds_handler;
 	    handler_val = ref_swig_cb(handler, sensor_set_thresholds_cb);
@@ -7690,6 +7696,8 @@ ipmi_args_t *alloc_parse_args(argarray *args);
 	rv = ipmi_sensor_set_thresholds(self, th, sensor_cb, handler_val);
 	if (rv && handler_val)
 	    deref_swig_cb_val(handler_val);
+    out_err_free:
+	free(th);
     out_err:
 	IPMI_SWIG_C_CB_EXIT
 	return rv;

commit 1460986f8dbf9566ca925767062eee4db6b66042

lanserv: Don't free app in persist_init()
app is used in later in get_fname(). This issue was introduced in
098038d46fb263, causing a crash at startup.

Signed-off-by: Alan Ott <alan@softiron.co.uk>

lanserv/persist.c
@@ -119,7 +119,6 @@ persist_init(const char *papp, const char *instance, const char *ibasedir)
     strcpy(dname, basedir);
     strcat(dname, "/");
     strcat(dname, app);
-    free(app);
     strcat(dname, "/");
     if (dname[0] == '/')
 	n = strchr(dname + 1, '/');